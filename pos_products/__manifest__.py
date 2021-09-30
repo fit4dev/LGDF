f898f3f8ecfb58f1efcbf65a2c7db1445f749226
Nery Caballero <ncaba@DESKTOP-THHI6CE>
2021-09-30 09:50:32 +0200

    Created pos_products module, initial commit

---
+++ pos_products/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Le Gout du Frais',
+    'category': 'Inventory',
+    'summary': 'The specific and easy-to-use system in Odoo allows addons features on the web.',
+    'description': """ The specific and easy-to-use system in Odoo allows addons features on the web. """,
+    'depends': ['base','product'],
+    'data': [],
+    'installable': True,
+    'auto_install': False
+}
