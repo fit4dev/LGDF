06f544fa98d9f03b45deb8c89981781bfd8472bc
Nery Caballero <ncaba@DESKTOP-THHI6CE>
2021-09-30 14:24:32 +0200

    Created model and view for custom-field Margin

--- pos_products/__manifest__.py
+++ pos_products/__manifest__.py
@@ -5,8 +5,8 @@
     'category': 'Inventory',
     'summary': 'The specific and easy-to-use system in Odoo allows addons features on the web.',
     'description': """ The specific and easy-to-use system in Odoo allows addons features on the web. """,
-    'depends': ['base','product'],
-    'data': [],
+    'depends': ['product'],
+    'data': ['views/product_template_form_inherited.xml'],
     'installable': True,
     'auto_install': False
 }
