705ab227dfcb8c614dd19beed0369492ced90c73
Nery Caballero <ncaba@DESKTOP-THHI6CE>
2021-09-30 16:37:07 +0200

    Added compute functionality to Margin field.

--- pos_products/models/products.py
+++ pos_products/models/products.py
@@ -6,4 +6,7 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    product_margin = fields.Float(string='Margin')
+    product_margin = fields.Float(string='Margin', compute='_compute_product_margin')
+
+    def _compute_product_margin(self):
+        self.product_margin = self.list_price - self.standard_price
